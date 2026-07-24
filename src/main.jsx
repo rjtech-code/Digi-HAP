a32cfa1b700d62eaa4ca28ed02bded417359e166
Mayank Maharshi <mayankmaharshi01@gmail.com>
2026-07-24 13:45:30 +0530

    merged

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
-=======
->>>>>>> 61d0154e21c4297d53d478e3e66a33a52fe97745
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
       <App />
     </BrowserRouter>
-=======
-    <App />
->>>>>>> 61d0154e21c4297d53d478e3e66a33a52fe97745
   </StrictMode>,
 )
